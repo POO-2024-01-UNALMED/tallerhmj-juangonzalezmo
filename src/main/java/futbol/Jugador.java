6cbdf0b1cf4783d6761295bdc11f0a2c356e2f82
juangonzalezmo <juagonzalezmo@unal.edu.co>
2024-08-13 22:27:56 -0500

    Version 1
    Se implementa toda la lógica del programa

---
+++ src/main/java/futbol/Jugador.java
@@ -0,0 +1,52 @@
+package futbol;
+
+public class Jugador extends Futbolista{
+	
+	
+	public short golesMarcados;
+	public byte dorsal;
+	
+	public Jugador(String nombre, int edad, String posicion, short golesMarcados, byte dorsal){
+		
+		super(nombre,edad,posicion);
+		this.golesMarcados = golesMarcados;
+		this.dorsal = dorsal;
+		
+	}
+	
+	public Jugador(String nombre, int edad, String posicion) {
+		
+		super(nombre,edad,posicion);
+		this.golesMarcados = 289;
+		this.dorsal = 7;
+	}
+	
+
+	public int compareTo(Futbolista f) {
+		
+		return Math.abs(this.getEdad()-f.getEdad());
+		
+	}
+	
+	public String toString() {
+		
+		return "El futbolista " + this.getNombre() + " tiene " + this.getEdad() + ", y juega de " + this.getPosicion() +
+		" con el dorsal " + dorsal + ". Ha marcado " + golesMarcados;
+	}
+	
+	
+	public boolean jugarConLasManos() {
+		
+		return false;
+	}
+
+	@Override
+	public int compareTo(Object o) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+
+
+
+}
---
+++ src/main/java/futbol/Portero.java
@@ -0,0 +1,41 @@
+package futbol;
+
+public class Portero extends Futbolista{
+	
+	public short golesRecibidos;
+	public byte dorsal;
+	
+	Portero(String nombre, int edad, short golesRecibidos, byte dorsal){
+		
+		super(nombre,edad,"Portero");
+		this.golesRecibidos = golesRecibidos;
+		this.dorsal = dorsal;
+		
+	}
+	
+	public String toString() {
+		
+		return "El futbolista " + this.getNombre() + " tiene " + this.getEdad() + ", y juega de " + this.getPosicion() +
+		" con el dorsal " + dorsal + ". Le han marcado " + golesRecibidos;
+	}
+	
+	public int compareTo(Portero f) {
+		
+		return Math.abs(this.getEdad()-f.getEdad());
+		
+	}
+
+	
+	public int compareTo(Object o) {
+		
+		return 0;
+	}
+
+	
+	public boolean jugarConLasManos() {
+		
+		return true;
+	}
+	
+	
+}
